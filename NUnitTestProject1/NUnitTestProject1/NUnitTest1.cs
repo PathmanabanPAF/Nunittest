5974bdd3dfa01ad0c48bfc859b4f6dbb8addb036
PathmanabanPAF <pathmanaban.govindhan@gmail.com>
2020-11-29 15:29:30 +0530

    test

---
+++ NUnitTestProject1/NUnitTestProject1/NUnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+//using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    [TestFixture]
+    public class NUnitTest1
+    {
+        [Test]
+        public void second_test()
+        {
+            Console.WriteLine("Class 2: Testing 1");
+
+        }
+
+        [Test]
+        public void second_test_test()
+        {
+            Console.WriteLine("Class 2: Testing 1");
+        }
+    }
+}
---
+++ NUnitTestProject1/NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.IE;
+using System;
+//using NUnit.Framework;
+
+namespace NUnitTestProject1
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        
+        IWebDriver driver;
+     
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("Testing 1");
+            driver.FindElement((By.Name("q"))).SendKeys("test");
+
+            System.Threading.Thread.Sleep(6000);
+
+
+
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Console.WriteLine("Testing 2");
+
+        }
+
+        [SetUp]
+        public void login_test()
+        {
+            Console.WriteLine("Setup 1");
+            //driver = new InternetExplorerDriver();
+            driver = new ChromeDriver("F:\\Test_vs\\Drivers");
+            //string testpath = "F:\\Test_vs\\Drivers";
+            //driver = new ChromeDriver(Environment.GetEnvironmentVariable("testpath"));
+            //driver = new ChromeDriver(testpath);
+            driver.Navigate().GoToUrl("http://www.google.com");
+        }
+
+        [TearDown]
+        public void close_browser()
+        {
+            Console.WriteLine("closing browser 1");
+            driver.Quit();
+        }
+    }
+}
